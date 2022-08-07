c4bb71bcbb85b32a94b2eebbb564b4c2e641be20
GolubevAndrey <golubev_andrei87@mail.ru>
2022-08-08 02:23:24 +0300

    Первое задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        1 Задание
+        String fistName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = fistName + " " + middleName + " " + lastName;
+
+        System.out.println("fullName = " + fullName);
     }
 }
