dec51d9abaced70055cc4c26835dcb7077c472b0
GolubevAndrey <golubev_andrei87@mail.ru>
2022-08-08 08:48:12 +0300

    Второе задание

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+
 //        1 Задание
+
         String fistName = "Ivanov";
         String middleName = "Ivan";
         String lastName = "Ivanovich";
         String fullName = fistName + " " + middleName + " " + lastName;
 
         System.out.println("fullName = " + fullName);
+
+//        2 Задание
+
+        System.out.println(fullName.toUpperCase());
+
     }
 }
